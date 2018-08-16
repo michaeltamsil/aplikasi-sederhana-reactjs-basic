bdaf798617f40586a59eb1514e2f707e01be3023
michael tamsil <michaeltamsil@gmail.com>
2018-08-16 12:16:08 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+//import React from 'react';
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            nama: ''
+        }
+        
+        this.field = {
+            nama : React.createRef()
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick (ev) {
+        console.log(ev)
+        //alert(`nama anda adalah ${this.field.nama.current.value}`)
+        let nama = this.field.nama.current.value
+        this.setState({
+            nama
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to Reactjs</h1>
+                </header>
+                <p className="App-intro">
+                  To get started, edit <code>src/App.js</code> and save to reload.
+                    <div>
+                        <input type="input" name="nama" ref={this.field.nama}/>
+                        <button type="button" onClick={this.handleClick}>Kirim</button>
+                    </div>
+                    <h1>{this.state.nama}</h1>
+                </p>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
